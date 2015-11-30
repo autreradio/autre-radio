5558f2e2cd05a4f8a407952f220b6dcc87f0a07c
autreradio <prog@lautreradio.fr>
2015-11-30 13:13:35 +0100

    www

---
+++ js/frap/Flux.js
@@ -0,0 +1,36 @@
+Flux = {
+
+    _url_flux : 'lautreradio.js',//http://80.82.229.204/datasun/bocetoplayer/frap.js',
+
+    _frap_flux : '',
+
+    init : function() {
+        this.getFlux();
+    },
+	
+	getFlux : function() {
+		$(document).ready(function() {
+			$.ajax({ 
+				type: 'GET',
+				url: Flux._url_flux,
+				timeout: 3000,
+				success: function(data) {
+					eval('Flux._frap_flux = '+data);
+					if(Flux._frap_flux) {
+						Podcasts.init();
+						//Videos.init();
+						Api.init();
+						Player.init();
+						//Direct.init();
+					} else {
+						alert('connexion error flux');
+					}
+				},
+				error: function() {
+					console.log('error de connexion au FLUX');
+				}
+			});
+		}); //fin script Jquery  
+    }
+
+}
